226de920aed718ac88fc0c7238550bd93ee2116e
Andrei <andreylyashuk147@gmail.com>
2026-04-10 06:43:31 +0300

    Update db_interface.h

--- includes/db_interface.h
+++ includes/db_interface.h
@@ -1,19 +1,19 @@
 #pragma once
 #include <string>
 #include <vector>
-#include <functional>
-#include <cstdint>
 #include <memory>
+#include <functional>
 #include <sqlite3.h>
 
 class IDBManager {
 public:
     virtual ~IDBManager() = default;
-    virtual bool init(const std::string& db_path) = 0;
+    virtual bool init(const std::string& path) = 0;
     virtual bool exec(const std::string& sql) = 0;
-    virtual bool query(const std::string& sql, 
-                       const std::vector<std::string>& params,
+    virtual bool query(const std::string& sql, const std::vector<std::string>& params,
                        std::function<bool(int, char**, char**)> callback) = 0;
     virtual bool insertBlob(const std::string& sql, const std::vector<uint8_t>& data) = 0;
-    virtual std::vector<uint8_t> readBlob(const std::string& sql, int trawler_id) = 0;
+    virtual std::vector<uint8_t> readBlob(const std::string& sql, int id) = 0;
 };
+
+std::shared_ptr<IDBManager> createDB();
