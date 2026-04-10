db0bb6d3501ce9f46ecb36b83acf12b83e338ffa
Andrei <andreylyashuk147@gmail.com>
2026-04-10 06:44:23 +0300

    Update models.h

--- includes/models.h
+++ includes/models.h
@@ -3,14 +3,20 @@
 #include <vector>
 
 struct User {
-    int id;
+    int id = 0;
     std::string username;
     std::string role;
     int crew_db_id = 0;
+    
+    User() = default;
+    User(int i, const std::string& u, const std::string& r, int c) 
+        : id(i), username(u), role(r), crew_db_id(c) {}
 };
 
 struct TripReport {
-    int trip_id;
-    std::string departure, return_date, bank_name;
-    double total_kg;
+    int trip_id = 0;
+    std::string departure;
+    std::string return_date;
+    std::string bank_name;
+    double total_kg = 0.0;
 };
